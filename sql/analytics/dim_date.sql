995874c168dd1bef0ac7589f56d980c75b546551
schylerwagner <schy.wagner@gmail.com>
2026-08-11 09:49:54 -0400

    Update dim_date.sql

--- sql/analytics/dim_date.sql
+++ sql/analytics/dim_date.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS analytics.dim_date;
 
 CREATE TABLE analytics.dim_date AS
 SELECT
-    generated_date::DATE AS calendar_date,
-    EXTRACT(YEAR FROM generated_date)::INTEGER AS year,
-    EXTRACT(QUARTER FROM generated_date)::INTEGER AS quarter,
-    EXTRACT(MONTH FROM generated_date)::INTEGER AS month_number,
+    CAST(generated_date AS DATE) AS calendar_date,
+    CAST(EXTRACT(YEAR FROM generated_date) AS INTEGER) AS year,
+    CAST(EXTRACT(QUARTER FROM generated_date) AS INTEGER) AS quarter,
+    CAST(EXTRACT(MONTH FROM generated_date) AS INTEGER) AS month_number,
     TO_CHAR(generated_date, 'FMMonth') AS month_name,
     TO_CHAR(generated_date, 'YYYY-MM') AS year_month,
-    EXTRACT(DAY FROM generated_date)::INTEGER AS day_of_month,
-    EXTRACT(ISODOW FROM generated_date)::INTEGER AS day_of_week_number,
+    CAST(EXTRACT(DAY FROM generated_date) AS INTEGER) AS day_of_month,
+    CAST(EXTRACT(ISODOW FROM generated_date) AS INTEGER) AS day_of_week_number,
     TO_CHAR(generated_date, 'FMDay') AS day_of_week_name
 FROM GENERATE_SERIES(
     DATE '2023-01-01',
